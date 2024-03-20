703edbc8468c0e92e0f9cc365792d3627efc1bb8
yeyaozu <yaozuye@zju.edu.cn>
2024-03-20 20:36:58 +0800

    debug to false

--- src/util.ts
+++ src/util.ts
@@ -10,7 +10,7 @@ import {
 	loadImageBlob, AppWithDesktopInternalApi, EditorInternalApi
   } from "./helpers"
 
-let DEBUG = true;
+let DEBUG = false;
 const SUCCESS_NOTICE_TIMEOUT = 1800;
 
 export const print=(message?: any, ...optionalParams: any[]) =>{
